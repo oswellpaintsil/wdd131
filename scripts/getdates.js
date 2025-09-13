28b3a1c30bcb8f472c900c542ef2c0c3beccb74d
oswellpaintsil <opaintsil@byupathway.edu>
2025-09-13 10:37:14 +0000

    Corrections made to week 01 assignement after grader feedback.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,27 @@ window.addEventListener('load', () => {
     year.innerHTML = new Date().getFullYear();
 });
 
-document.getElementById("lastModified").innerHTML=new Date(document.lastModified);
+// document.getElementById("lastModified").innerHTML=new Date(document.lastModified);
+
+// const lastMod = new Date(document.lastModified);
+//   document.getElementById("lastModified").innerHTML =
+//     "Last Modification: " + lastMod;
+
+// const lastMod = new Date(document.lastModified);
+//   document.getElementById("lastModified").innerHTML =
+//     "Last Modification: " + lastMod.toLocaleString();
+
+const lastMod = new Date(document.lastModified);
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  };
+  // format without comma
+  const formatted = lastMod.toLocaleString(undefined, options).replace(",", "");
+  
+  document.getElementById("lastModified").innerHTML =
+    "Last Modification: " + formatted;
